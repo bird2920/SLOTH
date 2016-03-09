8c186e6b5b28da4161e54fdbb5297f6c0b3c7034
bird2920 <bird2920@gmail.com>
2016-03-08 18:21:00 -0700

    new file:   .vs/Sloth/v14/.suo 	new file:   Sloth.sln 	new file:   Sloth/App.config 	new file:   Sloth/Program.cs 	new file:   Sloth/Properties/AssemblyInfo.cs 	new file:   Sloth/Properties/Settings.Designer.cs 	new file:   Sloth/Properties/Settings.settings 	new file:   Sloth/Sloth.csproj 	new file:   Sloth/Sloth.csproj.user 	new file:   Sloth/Sloth_TemporaryKey.pfx 	new file:   Sloth/bin/Debug/Sloth.application 	new file:   Sloth/bin/Debug/Sloth.exe 	new file:   Sloth/bin/Debug/Sloth.exe.config 	new file:   Sloth/bin/Debug/Sloth.exe.manifest 	new file:   Sloth/bin/Debug/Sloth.pdb 	new file:   Sloth/bin/Debug/Sloth.vshost.application 	new file:   Sloth/bin/Debug/Sloth.vshost.exe 	new file:   Sloth/bin/Debug/Sloth.vshost.exe.config 	new file:   Sloth/bin/Debug/Sloth.vshost.exe.manifest 	new file:   Sloth/bin/Debug/app.publish/Sloth.exe 	new file:   Sloth/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   Sloth/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Sloth/obj/Debug/Sloth.application 	new file:   Sloth/obj/Debug/Sloth.csproj.FileListAbsolute.txt 	new file:   Sloth/obj/Debug/Sloth.csprojResolveAssemblyReference.cache 	new file:   Sloth/obj/Debug/Sloth.exe 	new file:   Sloth/obj/Debug/Sloth.exe.manifest 	new file:   Sloth/obj/Debug/Sloth.pdb 	new file:   Sloth/obj/Debug/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   Sloth/obj/Debug/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   Sloth/obj/Debug/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   Sloth/tumblr_static_sticker_375x360.ico

---
+++ Sloth/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Sloth.Properties;
+
+namespace Sloth
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args == null) throw new ArgumentNullException(nameof(args));
+            try
+            {
+                Console.WriteLine("Sloth File Organizer \r\n");
+                Console.WriteLine("Enter extension pattern to search.");
+                Console.WriteLine("  Leave blank for *.*");
+                var pattern = Console.ReadLine();
+                if (pattern == string.Empty)
+                    pattern = Settings.Default.pattern;
+
+                try
+                {
+                    string pathUser = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    string pathDownload = Path.Combine(pathUser, "Downloads");
+
+                    Console.WriteLine("Enter input path");
+                    Console.WriteLine("  Leave blank for Downloads folder");
+                    var inputPath = Console.ReadLine();
+                    if (inputPath != null && inputPath == string.Empty)
+                        inputPath = pathDownload;
+                    if (inputPath != null)
+                    {
+                        var info = new DirectoryInfo(inputPath);
+
+                        Debug.Assert(pattern != null, "pattern != null");
+                        var files = info.GetFiles(pattern).OrderBy(p => p.CreationTime).ToArray();
+
+                        Console.WriteLine("Enter output path");
+                        Console.WriteLine("  Leave blank for Downloads folder");
+
+                        var outputPath = Console.ReadLine();
+                        if (outputPath != null && outputPath == string.Empty)
+                            outputPath = pathDownload + "\\" + "Sloth";
+
+                        Console.Write("Press any key to start \r\n");
+                        Console.ReadLine();
+
+                        foreach (var file in files)
+                        {
+                            if (file.Extension == ".lnk") continue;
+                            if (file.Extension == string.Empty) continue;
+                            var directory = Directory.CreateDirectory(outputPath + "\\" + file.Extension.Substring(1));
+                            Console.WriteLine(file.Name);
+                            File.Move(file.FullName, directory.FullName + "\\" + file.Name);
+                            //DirectoryInfo di = Directory.CreateDirectory(Properties.Settings.Default.OutputPath + "\\" + Convert.ToString(file.CreationTime.Year) + "\\" + Convert.ToString(file.CreationTime.Month));
+                        }
+                    }
+                }
+                catch (IOException exception)
+                {
+
+                    Console.WriteLine(exception.ToString());
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.ToString());
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Finished - Press enter to quit");
+            Console.Read();
+        }
+    }
+}
